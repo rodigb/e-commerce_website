bab9a2295af79c8f1514ff26ace3cd603cfce1d9
rodigb <44157518+rodigb@users.noreply.github.com>
2021-07-26 12:48:03 +0100

    Add files via upload

---
+++ shellscript.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt-get install git
+mkdir ~/git
+cd ~/git
+git clone https://github.com/rodigb/e-commerce_website.git
+sudo mv /home/rodi/git /opt/lampp/htdocs
+
---
+++ xampinstallscript.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+sudo apt-get install git #installing git
+mkdir ~/git #making the directory for the website
+cd ~/git #navigating to it
+git clone https://github.com/rodigb/e-commerce_website.git #downloading the website from github
+
+mkdir ~/xampinstall
+cd ~/xampinstall #make directory to install xampp
+wget https://www.apachefriends.org/xampp-files/8.0.8/xampp-linux-x64-8.0.8-1-installer.run #download link
+chmod 755 xampp-linux-x64-8.0.8-1-installer.run
+ls -l xampp-linux-x64-8.0.8-1-installer.run
+sudo ./xampp-linux-x64-8.0.8-1-installer.run #run the installer
+
